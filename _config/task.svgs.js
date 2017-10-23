e6cdb4e94a8e9ac108da2a10a5e1dfd14c10dee1
tawashley <trisashley@gmail.com>
2017-10-23 15:39:26 +0100

    feat: Update default svg config to include scss render, template file

--- _config/task.svgs.js
+++ _config/task.svgs.js
@@ -2,28 +2,36 @@
 
 'use strict';
 
+var path = require('path');
+
 function getTaskConfig(projectConfig) {
 	var taskConfig = {
 		svgSrc: projectConfig.paths.src.svgs + '**/*.svg',
 		outDir: '',
-
 		settings: {
-		    "dest": "",
-		    "mode": {
-		        "css": {
-		        	"dest": projectConfig.paths.src.sass + "/_objects",
-		        	"sprite": process.cwd() + '/' + projectConfig.paths.dest.svgs + '/svgsprite.svg',
-		            "render": {
-		            	"scss": {
-		            		'dest': "_objects.svgsprite.scss"
-		            	}
-		            },
-		           	"layout": "packed",
-		           	"prefix": ".svgsprite--%s",
-		           	"dimensions": "__dimensions",
-		           	"bust": true
-		        }
-	    	}
+		    dest: '', // If using modes, leave this empty and configure on a mode level - determines where to put the SVG symbol/css sprite/def
+		    mode: {
+				css: {
+					dest: projectConfig.paths.src.sass + '/_tools', // where it specifically puts the css file (not where it'd put the def / symbol etc)
+					sprite: path.join(process.cwd(), projectConfig.paths.dest.images, '/sprite.svg'), // where it puts the svg file and what it names it
+					layout: 'packed', // default - layout of svg in the sprite
+					prefix: '.svgsprite--%s', // custom - prefix for svg classes
+					dimensions: '__dimensions', // custom - suffix for dimensions class e.g. .svg--hamburger__dimensions
+					bust: false, // default - cache busting,
+					render: {
+						scss: { // make it render scss and not css
+							dest: '_tools.svg-sprite.scss', // scss file name
+							template: path.join(process.cwd(), projectConfig.paths.src.images, '/svg-sprite-sass.tpl')
+						}
+					}
+				}
+	    	},
+			shape: {
+				spacing: { // Spacing related options
+					padding: 4, // Padding around all shapes
+					box: 'content' // Padding strategy (similar to CSS `box-sizing`)
+				}
+			}
 	    }
 
 	}
