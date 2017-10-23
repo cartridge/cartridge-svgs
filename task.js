b1dddae1276a462e3b3210ea3bab71999cc0cf35
tawashley <trisashley@gmail.com>
2017-10-23 17:31:39 +0100

    feat: Remove uneeded config

--- task.js
+++ task.js
@@ -21,28 +21,6 @@ module.exports = function(gulp, projectConfig, tasks) {
 	// Task Config
 	var taskConfig = require(path.resolve(process.cwd(), projectConfig.dirs.config, 'task.' + TASK_NAME + '.js'))(projectConfig);
 
-	var svgSrc       = taskConfig.svgSrc;
-	var outDir       = taskConfig.outDir;
-	var config       = {
-	    "dest": taskConfig.settings.dest,
-	    "mode": {
-	        "css": {
-	        	"dest": taskConfig.settings.mode.css.dest,
-	        	"sprite": taskConfig.settings.mode.css.sprite,
-	            "render": {
-	            	"scss": {
-	            		'dest': taskConfig.settings.mode.css.render.scss.dest
-	            	}
-	            },
-	           	"layout": taskConfig.settings.mode.layout,
-	           	"prefix": taskConfig.settings.mode.prefix,
-	           	"dimensions": taskConfig.settings.mode.dimensions,
-	           	"bust": taskConfig.settings.mode.bust
-	        }
-	    }
-	};
-
-
 	/* --------------------
 	*	MODULE TASKS
 	* ---------------------*/
